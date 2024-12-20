c107ada2540a75c8c66e152efcbb1327a1784aa8
daniilkimstach <daniilkimstachp@gmail.com>
2024-12-20 19:45:30 +0300

    Init commit

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+# Лабораторная работа 2 по дисциплине МРЗвИС
+# Выполнена студентом группы 121702 БГУИР Кимстач Д.Б.
+# Реализация модели двунаправленной ассоциативной памяти
+# Вариант 11
+# Ссылки на источники:
+# https://rep.bstu.by/bitstream/handle/data/30365/471-1999.pdf?sequence=1&isAllowed=y
+
+
 import numpy as np
 
 from src.bidirectional_associative_memory import BAM, image_beautiful_print
@@ -14,12 +22,10 @@ if __name__ == '__main__':
 
     bam.train(alphabet_inputs, alphabet_outputs)
 
-    # Демонстрация ассоциаций
     for input_symbol, output_symbol in associations.items():
         print(f"Входной образ ({input_symbol}):")
         image_beautiful_print(input_patterns[input_symbol], 4, 4)
 
-        # Используем recall для ассоциации выходных данных
         predicted_input, predicted_output = bam.recall(input_data=np.array(input_patterns[input_symbol]))
         print(f"\nАссоциированный выходной образ ({output_symbol}):")
         image_beautiful_print(predicted_output, 4, 4)
@@ -28,7 +34,6 @@ if __name__ == '__main__':
         image_beautiful_print(predicted_input, 4, 4)
         print()
 
-    # Тест с поврежденными данными
     print("Поврежденная буква А")
     damaged_A = [1, -1, 1, -1, 1, -1, -1, 1, 1, 1, 1, 1, 1, -1, -1, -1]
     image_beautiful_print(damaged_A, 4, 4)
